eebb1cb0ff8764e3498ee3563a3b7b8d47401827
Matthew Pocock <turingatemyhamster@gmail.com>
2023-10-10 03:11:30 +0100

    Tinkering with collatz numerals

--- numbers.lean
+++ numbers.lean
@@ -1,4 +1,5 @@
 import Mathlib.Tactic
+import Mathlib
 
 /-!
 # Introduction
@@ -44,22 +45,35 @@ The `zero` terminal ends the binary numeral.
 However, the outermost constructor is the least significant, making the zero the most significant.
 So we have, in effect, got a backwards binary numeral.
 
+A simple way to write these numerals is in the form `z10` where `z` represents the terminal `zero`,
+1 represents an `odd_of` and 0 represents an `even_of`, applying the constructors to the left.
+
+The length of the number is the count of `even_of` and `odd_of` constructors.
+So the length of `z10` is 2.
+
 Where this differs from binary is that we can substitute the `zero` terminal with any `n₀`,
 making the evaluation of these numerals equivalent to computing a linear function of `n₀`.
 This evaluates to the corresponding binary numeral when `n₀=0`.
+For a collatz numeral `z n₀` for the numeral rewritten in this manner, and `z 0` for the corresponding numeric value.
+More generally, a z digit is the class `(z n₀) mod (2^p) = (z 0) ` where `p` is the length of the numeral.
+
+This means that our collatz numerals differ from normal binary numerals, in that leading zeros are significant.
+`z1` is a different numeral from `z01`, as `z1` is the numbers `2n+1`, where as `z01` is the numbers `4n+1`.
 -/
 
 /--!
 A collatz numeral.
 -/
 inductive ColN where
-    | zero      : ColN          -- zero, also leading zero
+    | zero      : ColN          -- zero, most-significant-figure terminal
     | even_of   : ColN → ColN   -- the even number that is twice the previous
     | odd_of    : ColN → ColN   -- the odd number that is one more than twice the previous
 deriving Repr
 
 -- The z0 numeral is equivalent to 0.
 def z0 := ColN.zero
+
+def z0' := ColN.even_of ColN.zero
 
 -- The z1 numeral is equvalent to 1.
 def z1 := ColN.odd_of ColN.zero
@@ -86,6 +100,64 @@ def ColN.eval {α : Type} [HMul ℕ α α] [HAdd ℕ α α] (cn: ColN) (n : α) 
 | ColN.zero => n
 | ColN.even_of cm => 2 * (ColN.eval cm n)
 | ColN.odd_of cm => 1 + 2 * (ColN.eval cm n)
+
+@[simp]
+theorem coln_eval_zero : ColN.zero.eval = id := rfl
+
+@[simp]
+theorem coln_eval_even_of_zero : (ColN.even_of ColN.zero).eval = (2 * .) := rfl
+
+@[simp]
+def append (a b : ColN) : ColN := match b with
+| ColN.zero => a
+| ColN.even_of c => ColN.even_of (append a c)
+| ColN.odd_of c => ColN.odd_of (append a c)
+
+@[simp]
+theorem eval_append {a b : ColN} {n : ℕ}: (append a b).eval n = b.eval (a.eval n) := by
+  induction b with
+  | zero => rfl
+  | even_of cn h =>
+    simp
+    assumption
+  | odd_of cn h =>
+    simp
+    assumption
+
+
+@[simp]
+theorem eval_absorb_even_of_zero : cm = append (ColN.even_of ColN.zero) f → cm.eval 0 = f.eval 0 := by
+  rintro hcm
+  rw [hcm]
+  simp
+
+@[simp]
+theorem eval_eq {α : Type} [HMul ℕ α α] [HAdd ℕ α α] (a: ColN) (i j : α) : i = j → a.eval i = a.eval j := by
+  induction a with
+  | zero => simp
+  | even_of a h =>
+    intro ij_eq
+    have hij := h ij_eq
+    simp [hij]
+  | odd_of a h =>
+    intro ij_eq
+    have hij := h ij_eq
+    simp [hij]
+
+
+@[simp]
+theorem eval_even_of_zeros_eq_zero {p: ℕ} : ((Nat.iterate ColN.even_of p) ColN.zero).eval 0 = 0 := by
+  induction p with
+  | zero => rfl
+  | succ n h =>
+    rw [Function.iterate_succ_apply' ColN.even_of n ColN.zero]
+    simp
+    assumption
+
+theorem leading_zeros_same_c {cn : ColN} {p: ℕ} :
+  (append (ColN.even_of^[p] ColN.zero) cn).eval 0 = cn.eval 0 := by
+  simp
+
 
 /-!
 It behaves as expected:
@@ -222,6 +294,74 @@ theorem eval_nat_eq_eval_lineq1 (n : ℕ) (cn : ColN): ColN.eval cn n = (cn.eval
         simp
         ring
 
+/-!
+Lastly, we should be able to convert a natural number into an equivalant collatz number.
+-/
+def ColN.ofNat (n : ℕ) : ColN :=
+if n_is_zero : n = 0 then ColN.zero
+else
+  let l := n % 2 = 0
+  let r := n / 2
+  if l then ColN.even_of $ ColN.ofNat r
+  else ColN.odd_of $ ColN.ofNat r
+decreasing_by
+  simp_wf
+  exact Nat.bitwise_rec_lemma n_is_zero
+
+theorem ofNat_evals_to_nat_zero: (ColN.ofNat 0).eval 0 = 0 := by simp
+
+theorem ofnat_evals_to_nat_even(n : ℕ) (nz : ¬n=0) (nh : (ColN.ofNat n).eval 0 = n): (ColN.ofNat $ 2 * n).eval 0 = 2*n := by
+  rw [ColN.ofNat]
+  simp [nz, nh]
+
+theorem ofnat_evals_to_nat_odd(n : ℕ) (nh : (ColN.ofNat n).eval 0 = n) : (ColN.ofNat $ 2*n+1).eval 0 = 2*n+1 := by
+  rw [ColN.ofNat]
+  rw [Nat.odd_iff.mp]
+  simp [ColN.eval, Nat.add_comm, Nat.add_mul_div_left, Nat.div_eq_of_lt]
+  assumption
+  exact odd_two_mul_add_one n
+
+example (a b : ℕ) (ha : 0 < a) (hab : (a = 2*b)): (0 < b) := by
+  simp_all only [gt_iff_lt, zero_lt_mul_left]
+
+example (a b : ℕ) (ha : ¬ a = 0) (hab : (a = 2*b)): ¬ b = 0 := by
+  simp_all only [mul_eq_zero, false_or, not_false_eq_true]
+
+example (a : ℕ)(ha : ¬ a = 0) : 0 < a := by exact Nat.pos_of_ne_zero ha
+
+example (a b : ℕ)(ha : ¬ a = 0) (hab : (a = 2*b)) : b < a := by
+  have pa := Nat.pos_of_ne_zero ha
+  simp_all only [mul_eq_zero, false_or, gt_iff_lt, zero_lt_mul_left, lt_mul_iff_one_lt_left]
+
+theorem two_m_helper (m : ℕ) : m < 2 * m + 1 := by
+  have mle : m ≤ 2 * m := by
+    apply Nat.le_mul_of_pos_left
+    simp
+  exact Nat.lt_succ.mpr mle
+
+theorem ofNat_evals_to_nat(n : ℕ) : (ColN.ofNat n).eval 0 = n :=
+if nz : n = 0 then
+  by
+    simp [nz]
+else by cases (Nat.even_or_odd n) with
+  | inl even_n =>
+    have ⟨ m, hm ⟩ := even_n
+    have m2 : m + m = 2*m := by ring
+    have mnz : ¬ m = 0 := by simp_all only [mul_eq_zero, false_or, not_false_eq_true]
+    have mev := ofnat_evals_to_nat_even m mnz (ofNat_evals_to_nat m)
+    simp_all
+  | inr odd_n =>
+    have ⟨ m, hm ⟩ := odd_n
+    have mov := ofnat_evals_to_nat_odd m (ofNat_evals_to_nat m)
+    simp_all
+decreasing_by
+  have npos := Nat.pos_of_ne_zero nz
+  simp_wf
+  simp_all only [mul_eq_zero, false_or, gt_iff_lt, zero_lt_mul_left, lt_mul_iff_one_lt_left, two_m_helper]
+  
+  
+#exit
+
 /-! And with that, we're done representing the numerals that will be inputs to the collatz function.
 
 What has all this work bought us?
@@ -318,6 +458,7 @@ And now we can look at some examples of what collatz sequence numerals look like
 
 -- The linear equation in ℚ for z11 (=3).
 def cn3 := z11.eval le_1_ℚ
+#eval cn3
 
 #eval ColS.zero.eval le_1_ℚ 
 #eval ColS.zero.eval 1
@@ -411,7 +552,7 @@ We can represent this directly by rewriting the expression for tree from `z11` t
 The leading zero is saying that `n` is guaranteed even.
 -/
 
-def z011 := ColN.odd_of $ ColN.odd_of $ ColN.even_of ColN.zero
+def z011 := append z0' z11
 def cn03 := z011.eval le_1_ℚ
 /-!
 And just to check that z011 is another way to write 3, here it is.
@@ -431,7 +572,7 @@ Again, we see that while it fixes the problem with the 4 case, we now have issue
 More leading evens (zeros) will fix that though.
 -/
 
-def z0011 := ColN.odd_of $ z011
+def z0011 := append z0' z011
 def cn003 := z0011.eval le_1_ℚ
 
 #eval (cs_ududd.eval cn003)
@@ -439,8 +580,26 @@ def cn003 := z0011.eval le_1_ℚ
 #eval (cs_ududddd.eval cn003)
 
 
-def z00011 := ColN.odd_of $ z0011
+def z00011 := append z0' z0011
 def cn0003 := z00011.eval le_1_ℚ
+
+#eval z11
+#eval z011
+#eval z0011
+#eval z00011
+
+
+#eval cn3
+#eval cn03
+#eval cn003
+#eval cn0003
+
+
+
+#eval z11.eval le_0_ℚ
+#eval z011.eval le_0_ℚ
+#eval z0011.eval le_0_ℚ
+#eval z00011.eval le_0_ℚ
 
 #eval (cs_ududd.eval cn0003)
 #eval (cs_ududdd.eval cn0003)
@@ -451,4 +610,17 @@ And we're done.
 
 So, the collatz numeral `z00011` gives us an equation for all valid collatz chains where the smallest
 example is the chain from 3 to 1.
+More generally, it is the function:
+
+`32n+15 ↦ 
 -/
+
+def three_chain (n : ℚ) : ℚ × ℚ := ⟨ (z00011.eval n), (cs_ududddd.eval cn0003).eval n ⟩
+
+
+#eval three_chain 0
+#eval three_chain 1
+#eval three_chain 2
+#eval three_chain 3
+#eval three_chain 4
+#eval three_chain 5
